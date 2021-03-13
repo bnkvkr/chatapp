179b876434a0739b5da71dc075a6984cb9317111
Vishal <bnkvkr@gmail.com>
2021-03-13 21:00:42 +0530

    chat app

---
+++ public/client.js
@@ -0,0 +1,115 @@
+const socket = io()
+let name;
+var sent = new Audio("send.mp3");
+var rcv = new Audio("rcv.mp3");
+let textarea = document.querySelector('#textarea')
+let messageArea = document.querySelector('.message__area')
+let btn = document.querySelector(".btn");
+let room = document.querySelector(".room");
+let feedback = document.querySelector(".typing");
+do {
+    name = prompt('Please enter your name: ')
+} while(!name)
+// room.innerHTML = `${name}'s Chat Room`;
+socket.emit('joined',name);
+btn.addEventListener("click",(e)=>{
+    socket.emit('not-typing');
+   typeof(textarea.value);
+    if(textarea.value.trim().length != 0)
+    {
+        sendMessage(textarea.value)
+        sent.play();
+
+    }
+    
+    
+})
+textarea.addEventListener('keyup', function(e){
+    if(textarea.value.length > 0){
+      socket.emit('typing', name);
+    }
+    else
+    {
+        socket.emit('not-typing');
+    }
+    
+  });
+textarea.addEventListener('keyup', (e) => {
+   
+        if(e.key === 'Enter') {
+            
+            socket.emit('not-typing');
+          
+            if(e.target.value.trim().length !=0)
+            {
+
+                sendMessage(e.target.value);
+                sent.play();
+            }
+        }
+    
+})
+
+function sendMessage(message) {
+    let msg = {
+        user: name,
+        message: message.trim()
+    }
+    // Append 
+    appendMessage(msg, 'outgoing')
+    textarea.value = ''
+    scrollToBottom()
+
+    // Send to server 
+    socket.emit('message', msg)
+
+}
+
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className, 'message')
+
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+    `
+    mainDiv.innerHTML = markup
+    messageArea.appendChild(mainDiv)
+    
+}
+
+// Recieve messages 
+socket.on('message', (msg) => {
+    appendMessage(msg, 'incoming')
+    rcv.play();
+    scrollToBottom()
+})
+
+function scrollToBottom() {
+    messageArea.scrollTop = messageArea.scrollHeight
+}
+
+
+//joined
+socket.on('joined',(name)=>{
+    let mainDiv = document.createElement('div')
+    let className = 'joined'
+    mainDiv.classList.add(className, 'message')
+
+    let markup = `
+        
+        <p>${name} joined the chat</p>
+    `
+    mainDiv.innerHTML = markup
+    messageArea.appendChild(mainDiv)
+    scrollToBottom()
+})
+
+//typing''
+socket.on('typing', function(nam){
+    feedback.innerHTML = `${nam} is typing...`;
+});
+socket.on('not-typing', function(){
+    feedback.innerHTML = ``;
+});
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+
+const PORT = process.env.PORT || 3000
+
+http.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+// Socket 
+const io = require('socket.io')(http)
+
+io.on('connection', (socket) => {
+    console.log('Connected...')
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg)
+    })
+    socket.on('joined',(name)=>{
+        console.log(name)
+        socket.broadcast.emit('joined', name)
+    })
+    socket.on('typing',(name)=>{
+        
+        socket.broadcast.emit('typing', name)
+    })
+    socket.on('not-typing',()=>{
+       
+        socket.broadcast.emit('not-typing')
+    })
+   
+
+})
